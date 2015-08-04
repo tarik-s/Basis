6d2e0b4cd3700a50508bd416290b6866f3ca0314
Sergey Tararay <sergey.tararay@mailserver.ramka>
2015-08-04 20:26:38 +0300

    add remote class to sandbox

--- SandBoxes/Meticulous.SandBox/Program.cs
+++ SandBoxes/Meticulous.SandBox/Program.cs
@@ -7,10 +7,58 @@ using Meticulous.Threading;
 
 namespace Meticulous.SandBox
 {
+
+    public sealed class Remote<T>
+    {
+        private T _value;
+
+        public Remote(T value)
+        {
+            _value = value;
+        }
+
+        public static implicit operator Remote<T>(T value)
+        {
+            return new Remote<T>(value);
+        }
+
+        public static implicit operator T(Remote<T> value)
+        {
+            if (value == null)
+                return default(T);
+
+            return value.Value;
+        }
+
+        public event EventHandler<EventArgs> Changed;
+
+        public T Value
+        {
+            get { return _value; }
+        }
+    }
+
+    public class RemoteFieldAttribute : Attribute
+    {
+        public RemoteFieldAttribute(string name)
+        {
+        }
+    }
+
     class Program
     {
+        [RemoteField("ServerVersion")]
+        private static readonly Remote<string> _serverVersion = default(string);
+
+
+
         static void Main(string[] args)
         {
+            _serverVersion.Changed += delegate(object sender, EventArgs eventArgs)
+            {
+                
+            };
+
             using (var q = ExecutionQueue.Create())
             {
                 q.Post(() =>
